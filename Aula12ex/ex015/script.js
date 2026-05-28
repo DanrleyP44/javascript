98d713070b11d7ceb6e6527b7556507f0aae6f2c
Danrley Pereira <danrleypereira294@proton.me>
2026-05-28 19:08:54 -0300

    Conclusão exercício idade

--- Aula12ex/ex015/script.js
+++ Aula12ex/ex015/script.js
@@ -1,21 +1,55 @@
-function carregar() {
-  let msg = document.getElementById('msg');
-  let foto = document.getElementById('imagem');
-  let hora = new Date().getHours();
+function verificar() {
+  let ano = new Date().getFullYear();
 
-  msg.innerHTML = `Agora são ${hora} horas`;
+  let formAno = document.getElementById('txtano');
+  let resposta = document.getElementById('res');
 
-  if (hora >= 0 && hora < 12) {
-    // BOM DIA
-    foto.src = '../manhã.jpg';
-    document.body.style.background = '#025677';
-  } else if (hora >= 12 && hora <= 18) {
-    // BOA TARDE
-    foto.src = '../tarde.jpg';
-    document.body.style.background = '#FC9E58';
+  let container = document.getElementById('container');
+
+  if (formAno.value.length === 0 || formAno.value > ano) {
+    alert('[ERRO] Verifique os dados e tente novamente');
   } else {
-    // BOA NOITE
-    foto.src = '../noite.jpg';
-    document.body.style.background = '#09203A';
+    let vsex = document.getElementsByName('sexo');
+    let idade = ano - Number(formAno.value);
+
+    let img = document.createElement('img');
+    img.setAttribute('id', 'foto');
+
+    let gen = '';
+    if (vsex[0].checked) {
+      gen = 'Homem';
+      if (idade >= 0 && idade < 10) {
+        //Criança
+        img.src = './imagens/crianca-m.jpg';
+      } else if (idade < 21) {
+        //Jovem
+        img.src = './imagens/young-m.jpg';
+      } else if (idade < 51) {
+        //Adulto
+        img.src = './imagens/adult-m.jpg';
+      } else {
+        //IDOSO
+        img.src = './imagens/old-m.jpg';
+      }
+    } else if (vsex[1].checked) {
+      gen = 'Mulher';
+      if (idade >= 0 && idade < 10) {
+        //Criança
+        img.src = './imagens/crianca-f.jpg';
+      } else if (idade < 21) {
+        //Jovem
+        img.src = './imagens/young-f.jpg';
+      } else if (idade < 51) {
+        //Adulto
+        img.src = './imagens/adult-f.jpg';
+      } else {
+        //IDOSO
+        img.src = './imagens/old-f.jpg';
+      }
+    }
+
+    resposta.textContent = `Verificamos ${gen} com ${idade} anos`;
+    resposta.style.textAlign = 'center';
+    container.appendChild(img);
   }
 }
